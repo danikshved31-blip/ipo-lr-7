f690ee9af288e73a6cedc43fc6c54b58f7b17d4c
danchik <danikshved31@gmail.com>
2025-11-20 12:42:15 +0300

    izi

--- task_2/main.py
+++ task_2/main.py
@@ -1,1 +1,13 @@
-
+import json
+with open("dump.json", "r", encoding="utf - 8") as izi_pizy_lemon_squizue:
+    a = json.load(izi_pizy_lemon_squizue)
+    qualiti = input("введите номер квалификации: ").strip()
+    for word in a:
+        if word.get("model") == "data.skill":
+            if word.get("fields", {}).get("code") == qualiti:
+                print(f"======================== Найдено ==========================")
+                print(f"{word["fields"]["title"]}")
+                break
+            else:
+                print("===========================Не найдено ============================")
+                break
